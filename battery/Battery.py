4b1d0d80de2498389fed94e5cebee32a5c2b0295
Nahom32 <senaynahom00@gmail.com>
2023-06-04 22:40:37 +0300

    configured project to the requirements

---
+++ battery/Battery.py
@@ -0,0 +1,6 @@
+class Battery:
+    def __init__(self):
+        pass
+    def needs_service(self):
+        pass
+    
--- car.py
+++ car.py
@@ -1,10 +1,12 @@
-from abc import ABC, abstractmethod
+from engine.Engine import Engine
+from battery.Battery import Battery
+from Serviceable import Serviceable
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
-    @abstractmethod
+class Car(Serviceable):
+    def __init__(self, engine: Engine, battery: Battery):
+        self.engine = engine
+        self.battery = battery
     def needs_service(self):
-        pass
+        return self.engine.needs_service() and self.battery.needs_service()
+    
